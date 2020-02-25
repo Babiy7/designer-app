5ac3fc2939ca173c1d3620263158bc9a4e2073a5
Babiy <olegbabiy77@gmail.com>
2020-02-25 20:02:02 +0200

    Creared news reducer.

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import modalReducer from "../reducers/modal";
+import newsReducer from "../reducers/news";
 
 const rootReducer = combineReducers({
-  modal: modalReducer
+  modal: modalReducer,
+  news: newsReducer
 });
 
 export default rootReducer;
---
+++ src/store/reducers/news.js
@@ -0,0 +1,53 @@
+const initialState = {
+  news: [
+    {
+      title: "ten the best app in 2017",
+      category: "category",
+      date: "10 June",
+      text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pellentesque eu enim eget luctus. Sed augue felis, facilisis et elementum vitae, aliquam sit amet ante. Sed iaculis eros sem, elementum consequat."
+    },
+    {
+      title: "website inspiration",
+      category: "category",
+      date: "17 June",
+      text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pellentesque eu enim eget luctus. Sed augue felis, facilisis et elementum vitae, aliquam sit amet ante. Sed iaculis eros sem, elementum consequat."
+    },
+    {
+      title: "changes in social media",
+      category: "category",
+      date: "27 June",
+      text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pellentesque eu enim eget luctus. Sed augue felis, facilisis et elementum vitae, aliquam sit amet ante. Sed iaculis eros sem, elementum consequat."
+    },
+    {
+      title: "changes in social media",
+      category: "category",
+      date: "27 June",
+      text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pellentesque eu enim eget luctus. Sed augue felis, facilisis et elementum vitae, aliquam sit amet ante. Sed iaculis eros sem, elementum consequat."
+    },
+    {
+      title: "changes in social media",
+      category: "category",
+      date: "27 June",
+      text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pellentesque eu enim eget luctus. Sed augue felis, facilisis et elementum vitae, aliquam sit amet ante. Sed iaculis eros sem, elementum consequat."
+    },
+    {
+      title: "changes in social media",
+      category: "category",
+      date: "27 June",
+      text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pellentesque eu enim eget luctus. Sed augue felis, facilisis et elementum vitae, aliquam sit amet ante. Sed iaculis eros sem, elementum consequat."
+    }
+  ]
+};
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+    default:
+      return state;
+  }
+};
