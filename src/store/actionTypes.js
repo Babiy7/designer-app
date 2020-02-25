93f6c9ee9cb89267eb0ada021d9f64b630bbf658
Babiy <olegbabiy77@gmail.com>
2020-02-25 18:21:59 +0200

    Created help utilities

---
+++ src/store/actionTypes.js
@@ -0,0 +1,8 @@
+//Models
+export const OPEN_CALL = "OPEN_CALL";
+export const OPEN_PROJECT = "OPEN_PROJECT";
+export const OPEN_RESUME = "OPEN_RESUME";
+
+export const CLOSE_CALL = "CLOSE_CALL";
+export const CLOSE_PROJECT = "CLOSE_PROJECT";
+export const CLOSE_RESUME = "CLOSE_RESUME";
